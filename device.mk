f4db3e7fd47314fa33bf91805d502e99d2b70d67
Gabriel Biren <gbiren@google.com>
2022-10-28 18:54:04 +0000

    r5x: Use AIDL WiFi service
    Bug: 205044134
    Test: Presubmit tests
    Change-Id: Icdaa448e2ed59b638056593adb4bcd769f34220d

--- device.mk
+++ device.mk
@@ -508,7 +508,7 @@ PRODUCT_PACKAGES += \
 
 # Wifi
 PRODUCT_PACKAGES += \
-    android.hardware.wifi@1.0-service \
+    android.hardware.wifi-service \
     hostapd \
     libwifi-hal-ctrl \
     libwifi-hal-qcom \
